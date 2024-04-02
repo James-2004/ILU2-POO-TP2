d6312bc1e5e029c83e7789ebd14857533a4ce53d
James-2004 <jamessaba1804@gmail.com>
2024-04-02 12:56:56 +0200

    TP2-3

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,28 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
+		String[] infosMarche = controlAfficherMarche.donnerInfosMarche();
+		if(infosMarche.length == 0) {
+			System.out.println("Le marché est vide, revenez plus tard.");
+		} else {
+			StringBuilder result = new StringBuilder();
+			result.append(nomAcheteur);
+			result.append(", vous trouverez au marché :\n");
+			int i = 0;
+			while(i < infosMarche.length) {
+				result.append("- ");
+				result.append(infosMarche[i]);
+				i++;
+				result.append(" qui vend ");
+				result.append(infosMarche[i]);
+				i++;
+				result.append(" ");
+				result.append(infosMarche[i]);
+				i++;
+				result.append("\n");
+			}
+			System.out.println(result.toString());
+		}
 
 	}
 }
